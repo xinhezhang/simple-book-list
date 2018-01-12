032078f1bf5544ae23a5ca67e764bfd85367e4c4
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 16:08:53 -0800

    create BooksReducer for redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,13 @@
+export default function() {
+    return (
+        [
+            { title: 'Java' },
+            { title: 'C++' },
+            { title: 'Python' },
+            { title: 'HTML' },
+            { title: 'CSS' },
+            { title: 'JavaScript' },
+            { title: 'Matlab' },
+        ]
+    );
+}
