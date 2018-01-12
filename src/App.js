f18d3afc39c3bbf776d9f43f25385c40e637ef4d
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 16:24:11 -0800

    setup CSS file structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import './style/style.css'
+
 import Header from './components/header'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header />
+            </div>
+        );
+    }
 }
 
 export default App;
