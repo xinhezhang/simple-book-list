d4ca8117db5472ce19bfe11de52023bd528443da
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 20:38:40 -0800

    self test

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ import './style/style.css'
 import Header from './components/header'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: '',
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(keyword) {
+        console.log(keyword);
+        this.setState({
+            keyword,
+        });
+    }
     render() {
         return (
             <div className="App">
                 <Header />
+                <input
+                    value={this.state.keyword}
+                    onClick={(event) => this.onInputChange(event.target.value)}
+                />
             </div>
         );
     }
