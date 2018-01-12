a02b941eff9ac22eda5b5a1dab353c86982332b2
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 23:08:02 -0800

    create BookDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../style/style.css'
 
 import Header from './header';
 import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
     render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
             <div className="App">
                 <Header />
                 <BookList />
+                <BookDetail />
             </div>
         );
     }
