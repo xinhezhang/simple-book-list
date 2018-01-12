1eac5281114edca00e8461090ebe71a9ba4864d0
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 21:31:55 -0800

    add important comments for redux/react connection

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -6,13 +6,15 @@ class BookList extends Component {
         return this.props.books.map((book) => {
             return (
                 <li className="list-group-item" key={book.title}>
-                    {book.title}
+                    {book.title} <code>by</code> {book.author}
                 </li>
             );
         });
     }
 
     render() {
+        //console.log(this.props.test);   // "200"
+        //console.log(this.props.books);
         return (
             <ul className="list-group col-sm-4">
                 {this.renderList()}
@@ -21,12 +23,16 @@ class BookList extends Component {
     }
 }
 
+// This function connect react with redux
 // whatever return by this function will show up as props inside of BookList
+// whenever the state changes, this function will be called, and the new "books" will re-render
 function mapStateToProps(state) {
     return {
-        books: state.books,
+        test: 200,
+        books: state.books,     // "books" will be available for "this.props"
     };
 }
 
-// export container
+// connect(function)(component) -> container,
+// which is a component where the state contains in redux
 export default connect(mapStateToProps)(BookList);
