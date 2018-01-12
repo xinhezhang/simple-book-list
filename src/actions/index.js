b71511baad01c0988eb239148170233c19e61a33
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 22:41:37 -0800

    define selectBook's action type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 // This is a "action creator", which is just a function
+// it needs to return an action (an object with a type property)
 export function selectBook(book) {
-    console.log('A book has been selected: ', book.title);
+    //console.log('A book has been selected: ', book.title);
     return {
-        type: 'TBD',
-        book: book,
+        type: 'BOOK_SELECTED',  // an action must have a type, usually not a string
+        payload: book,          // payload is just more info of the action
     };
 }
