d3103aecb5afbba0624eedb4e36867b9454d5d9a
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 23:04:24 -0800

    create ActiveBook reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,10 @@
+// reducer will be called only when "action" occurs
+// "state" argument is not application state, only the state this reducer is responsible for
+export default function (state = null, action) {
+    switch (action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+    // originally is undefined, which is NOT allowed, so we set default to "null"
+    return state;
+}
