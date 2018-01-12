30e932714cf07a68e37e929063230f8c6375a1b0
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 23:11:02 -0800

    change BookDetail to a container

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render() {
         return (
             <div>
@@ -9,3 +10,11 @@ export default class BookDetail extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        book: state.activeBook,
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail);
